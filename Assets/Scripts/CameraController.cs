9846c1c4c1a48776805991257073a4f829414cb3
Birathesh-mano <bmano084@uottawa.ca>
2020-11-05 03:42:54 -0500

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour{
+
+
+    public Transform target;
+    public Vector3 offset;
+
+    public bool useOffsetValues;
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!useOffsetValues){
+        offset = target.position - transform.position;
+        }
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate(){
+
+        //get x pos of mouse and rotate target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0,horizontal,0);
+
+
+        //get y pos of mouse and rotate pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+
+        //limit up/down roto
+        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f){
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle){
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+
+
+
+
+        //move cam based on current rotation of target and org offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+
+        //transform.position = target.position - offset;
+
+        if(transform.position.y < target.position.y){
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+
+        transform.LookAt(target);
+        
+    }
+}
